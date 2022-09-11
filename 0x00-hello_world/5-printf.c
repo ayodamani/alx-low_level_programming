2fbb2e959db7aabd1d6015bff020da6d6f8fe2c3
ayodamani <ayodamani@gmail.com>
2022-09-11 03:52:01 -0700

    my first program on C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+   * main - Prints statement
+    *
+     * Description: a C program that prints exactly with proper grammar, but the
+      * outcome is a piece of art, followed by a new line.
+       *
+        * return - Return 0
+	 * Return: return 0
+	  *
+	   */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
