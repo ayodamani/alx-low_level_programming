bcf332446a1244de6b03077f7ac81ea2ba9c2c89
ayodamani <ayodamani@gmail.com>
2022-09-11 04:50:20 -0700

    my first prigram on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - Prints a statement
-* Description: A C program that prints a statement to the standard error.
-* Return: return 0
-*/
-
+ * main - prints exactly "and that piece of art is useful"
+ * -Dora Korpar, 2015-10-19",
+ * followed by new line, to the standard error
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19", 59);
+	return (1);
 }
