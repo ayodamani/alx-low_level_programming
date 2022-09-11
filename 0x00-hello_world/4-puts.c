46e3f1267d0af59ed9bdd587d05107cfd9e67b66
ayodamani <ayodamani@gmail.com>
2022-09-11 03:43:06 -0700

    my first program on C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints staactly Programming is like
+* building a multilingutement
+*
+* Description: This programme prints exal puzzle, followed by a new line.
+*
+* Return: return 0
+*
+*/
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
