09abb8aec1f284ce7965379826851aae6ab2e3f6
ayodamani <ayodamani@gmail.com>
2022-09-13 06:36:42 -0700

    my first program in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * print_alphabet-lowercase characters are printed to the screen
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+
+}
