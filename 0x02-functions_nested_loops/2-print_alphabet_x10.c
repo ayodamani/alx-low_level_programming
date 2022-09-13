b0a462d34fa8e36711db5b9c3df786af23409d0d
ayodamani <ayodamani@gmail.com>
2022-09-13 06:50:30 -0700

    my first program in C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times in a row
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
