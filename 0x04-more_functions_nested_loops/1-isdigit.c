d924119f3397ab24658ad42d5deec682897601f9
ayodamani <ayodamani@gmail.com>
2022-09-15 03:35:43 -0700

    working with debugging

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= '1' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
