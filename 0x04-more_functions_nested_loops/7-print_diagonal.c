a42e564162bc7e82c79aca5840cf2c5752ebcd46
ayodamani <ayodamani@gmail.com>
2022-09-15 05:19:40 -0700

    working with debugging

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,13 @@
 
 /**
  * print_diagonal- print space and slash symbol in configuration with int n
- *
+ * @n: number of times the character \ should be printed
  * Return - return 0
  */
 
 void print_diagonal(int n)
 {
-	if (n == 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -20,11 +20,11 @@ void print_diagonal(int n)
 		{
 			int j;
 
-			for (j = 1; j < i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
