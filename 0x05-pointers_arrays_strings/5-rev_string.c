9171ac60e19b1aff95ccc6ff777074ac7ec6ea0d
ayodamani <91778243+ayodamani@users.noreply.github.com>
2022-09-19 15:37:30 +0100

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * rev_string - prints a reverse string
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+    int length;
+}
