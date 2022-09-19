59ef6effcbce865f4f9573a78388debce6757f7f
ayodamani <91778243+ayodamani@users.noreply.github.com>
2022-09-19 15:44:52 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,26 +5,25 @@
  *
  *Return: void
  */
-
 void rev_string(char *s)
 {
-    char tmp;
-    int i, len, len1;
-    
-    len = 0;
-    len1 = 0;
+	char tmp;
+	int i, len, len1;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	len = 0;
+	len1 = 0;
 
-    len1 = len - 1;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    for (i = 0; i < len / 2; i++)
-    {
-        tmp = s[i];
-        s[i] = s[len1];
-        s[len1--] = tmp;
-    }
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
