237db73703e7e4a070cbbf24e4faca1d497db102
ayodamani <91778243+ayodamani@users.noreply.github.com>
2022-09-22 12:09:42 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
+    
 	for (j = 0; j < n; j++)
 	{
 		if (src[j] == '\0')
@@ -22,6 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = src[j];
 
 	}
+
 	while (j < n)
 	{
 		dest[j] = '\0';
