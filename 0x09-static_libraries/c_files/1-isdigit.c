8f4355defd7bf0361e8168df675ce21ebaa2eb02
ayodamani <91778243+ayodamani@users.noreply.github.com>
2022-10-01 04:57:32 +0100

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/c_files/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - evalue if character is digit.
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
