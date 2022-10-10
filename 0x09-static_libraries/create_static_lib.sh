1c4490795768fecf9bb052513c8a744867ea8da9
ayodamani <91778243+ayodamani@users.noreply.github.com>
2022-10-10 20:04:45 +0100

    structures

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+<<<<<<< HEAD
 gcc -c *.c
 ar rc liball.a *.o
 ranlib liball.a
+=======
+
+gcc -c *.c
+
+ar rc liball.a *.o
+
+ranlib liball.a
+
+
+>>>>>>> c9360e36f9b9e901e07c6ac27a41190707efb2c2
